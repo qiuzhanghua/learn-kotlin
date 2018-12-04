8b8dfadcbc7ae2fa479453e7e6db12f18369273a
邱张华 <qiuzhanghua@icloud.com>
2018-12-04 08:44:53 +0800

    spring boot -> 2.1.1

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,8 +1,10 @@
 object Deps {
     val junitVersion = "4.12"
-    val coroutinesVersion = "1.0.1"
-    val springBootVersion = "2.1.0.RELEASE"
+
     val kotlinVersion = "1.3.10"
+    val coroutinesVersion = "1.0.1"
+
+    val springBootVersion = "2.1.1.RELEASE"
 
     val shadowVersion = "4.0.2"
     val micronautVersion = "1.0.1"
