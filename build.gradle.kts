8a5b3c387676b501066f45f59b733f03adb592dc
邱张华 <qiuzhanghua@icloud.com>
2018-12-07 09:36:40 +0800

    kotlin 1.3.10 -> 1.3.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
 }
 
 subprojects {
--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,7 +1,7 @@
 object Deps {
     val junitVersion = "4.12"
 
-    val kotlinVersion = "1.3.10"
+    val kotlinVersion = "1.3.11"
     val coroutinesVersion = "1.0.1"
 
     val springBootVersion = "2.1.1.RELEASE"
