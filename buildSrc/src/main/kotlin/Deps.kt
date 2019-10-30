7f5801b6714192fea171199a388c69652bac0e5d
邱张华 <daniel@q.local>
2019-10-30 09:05:46 +0800

    update spring micronaut & ktor version

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -4,8 +4,8 @@ object Deps {
     val kotlinVersion = "1.3.50"
     val coroutinesVersion = "1.3.1"
 
-    val springBootVersion = "2.1.8.RELEASE"
+    val springBootVersion = "2.2.0.RELEASE"
 
     val shadowVersion = "5.1.0"
-    val micronautVersion = "1.2.2"
+    val micronautVersion = "1.2.5"
 }
