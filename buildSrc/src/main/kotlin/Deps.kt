b194344d26c4af5f076c26f6fbc0159665f544b3
Daniel <qiuzhanghua@icloud.com>
2020-03-05 08:51:58 +0800

    kotlin 1.3.61 -> 1.3.70 spring boot -> 2.2.5 mn -> 1.3.2

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,11 +1,11 @@
 object Deps {
-    val junitVersion = "4.12"
+    const val junitVersion = "4.12"
 
-    val kotlinVersion = "1.3.50"
-    val coroutinesVersion = "1.3.1"
+    const val kotlinVersion = "1.3.70"
+    const val coroutinesVersion = "1.3.3"
 
-    val springBootVersion = "2.2.0.RELEASE"
+    const val springBootVersion = "2.2.5.RELEASE"
 
-    val shadowVersion = "5.1.0"
-    val micronautVersion = "1.2.5"
+    const val shadowVersion = "5.2.0"
+    const val micronautVersion = "1.3.2"
 }
